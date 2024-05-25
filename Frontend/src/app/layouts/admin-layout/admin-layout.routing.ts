390ee0dc5f1cfc4cf17e05fe401b56b91231533e
amel967 <149878677+amel967@users.noreply.github.com>
2024-05-25 14:13:12 +0100

    Add files via upload

---
+++ Frontend/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,25 @@
+import { Routes } from '@angular/router';
+
+import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
+import { UserComponent } from '../../pages/user/user.component';
+import { TableComponent } from '../../pages/table/table.component';
+import { TypographyComponent } from '../../pages/typography/typography.component';
+import { IconsComponent } from '../../pages/icons/icons.component';
+import { MapsComponent } from '../../pages/maps/maps.component';
+import { NotificationsComponent } from '../../pages/notifications/notifications.component';
+import { UpgradeComponent } from '../../pages/upgrade/upgrade.component';
+import { AuthorComponent } from 'app/pages/author/author.component';
+import { ArticleComponent } from 'app/pages/article/article.component';
+
+export const AdminLayoutRoutes: Routes = [
+    { path: 'dashboard',      component: DashboardComponent },
+    { path: 'user',           component: UserComponent },
+    { path: 'table',          component: TableComponent },
+    { path: 'authors',          component: AuthorComponent },
+    { path: 'articles',          component: ArticleComponent },
+    { path: 'typography',     component: TypographyComponent },
+    { path: 'icons',          component: IconsComponent },
+    { path: 'maps',           component: MapsComponent },
+    { path: 'notifications',  component: NotificationsComponent },
+    { path: 'upgrade',        component: UpgradeComponent }
+];
